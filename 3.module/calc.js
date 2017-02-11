3d2b1e81804a4b99f1e3dedbce477957477a16c7
zhufengzhufeng <894918097@qq.com>
2017-02-11 10:56:07 +0800

    nrm

--- 3.module/calc.js
+++ 3.module/calc.js
@@ -4,4 +4,12 @@ var calc = {
     },
     '-': (a,b)=>a-b
 };
+module.exports = calc;
+/*var a = 1;
+var b = 2;
+exports.a = a;
+exports.b = b;*/
+// exports.calc = calc; √
+// module.exports.calc = calc; √
+// module.exports = calc; √
 //2中方式导出
