a6f8485dd1e72f20625b062164b4db591f399dd7
zhufengzhufeng <894918097@qq.com>
2017-02-11 16:14:01 +0800

    concat

--- 6.Buffer/2.buffer.js
+++ 6.Buffer/2.buffer.js
@@ -31,6 +31,32 @@ console.log(buffer.toString());
 var buffer = new Buffer(9);
 var buffer1 = new Buffer('我很');
 var buffer2 = new Buffer('帅');
-buffer2.copy(buffer);
-buffer1.copy(buffer);
-console.log(buffer.toString());
+var buffer3 = new Buffer('帅');
+buffer2.copy(buffer,6);
+buffer1.copy(buffer,0);
+console.log(buffer.toString()); //后两个参数默认不写表示从头拷贝到尾部
+
+//concat 返回一个新的数组
+console.log(Buffer.concat([buffer1,buffer2,buffer3]).toString());
+
+//myConcat 可以将多个buffer拼接到一起如果 长度过长截取有效长度，过短截取指定长度，不给默认全拼接
+Buffer.myConcat = function (list,totalLength) {
+    //1.判断totalLength是否传递
+    if(typeof totalLength=='undefined'){
+        //3.长度没有传 算出数组中的每个buffer的长度累加length 构建buffer在一个个copy进去
+        totalLength = 0;
+        list.forEach(function (item) {
+            totalLength+=item.length;
+        });
+    }
+    //2.如果传递构建一个大buffer将list中buffer一个个copy到buffer上，最后截取有效长度slice()
+    var buffer = new Buffer(totalLength);
+    var index = 0;
+    list.forEach(function (item) {
+        item.copy(buffer,index);
+        index+=item.length; //维护偏移量 下一次的拷贝 是上一次的考入的长度的累加
+    });
+    return buffer.slice(0,index);//index是最后考入的总长度
+    //4.如果过短不考虑
+};
+console.log(Buffer.myConcat([buffer1,buffer2,buffer3]).toString());
