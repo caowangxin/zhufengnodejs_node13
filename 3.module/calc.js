9f36e94d5ff3af8660081055eb9cf3949ecc067e
zhufengzhufeng <894918097@qq.com>
2017-02-11 10:12:17 +0800

    calc

---
+++ 3.module/calc.js
@@ -0,0 +1,7 @@
+var calc = {
+    '+':function (a,b) {
+        return a+b;
+    },
+    '-': (a,b)=>a-b
+};
+//2中方式导出
---
+++ 3.module/note.md
@@ -0,0 +1,4 @@
+## nodejs遵循commonjs规范
+- 如何创建一个模块(1个js文件就是一个模块)
+- 如果导出模块 exports /module.exports
+- 依赖别人的模块 require()
