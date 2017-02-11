4521bdf31ce19ca6033e467604380456ab37a904
zhufengzhufeng <894918097@qq.com>
2017-02-11 17:29:31 +0800

    copy

---
+++ 7.fs/4.copy.js
@@ -0,0 +1,11 @@
+//copy方法 克隆文件
+var fs = require('fs');
+//同步拷贝 readFileSync+writeFileSync
+function copySync(source,target) {
+    
+}
+//异步拷贝 readFile+writeFile
+function copy(source,target) {
+    
+}
+copy('./age.txt','./age1.txt');
